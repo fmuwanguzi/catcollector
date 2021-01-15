9e8c17cfeb0ee777bee35f8949a9a4b5c547226f
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2021-01-14 19:06:05 -0500

    added user

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Cat
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
 
 # Create your views here.
 # views are controllers
@@ -25,6 +26,12 @@ class CatCreate(CreateView):
     fields = '__all__'
     success_url = '/cats'
     
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect('/cats')
+    
 class CatUpdate(UpdateView):
     model = Cat
     fields = ['name', 'breed', 'description', 'age']
@@ -38,3 +45,10 @@ class CatDelete(DeleteView):
     model = Cat
     success_url = '/cats'
 
+
+#### USER #######
+
+def profile(request, username):
+    user = User.objects.get(username=username)
+    cats = Cat.objects.filter(user=user)
+    return render(request, 'profile.html', {'username': username, 'cats': cats})
